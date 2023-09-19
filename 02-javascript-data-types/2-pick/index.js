b1b37161507b408b9b7ceb845aec495ea879487d
Aleksei Vinter <lexawin@gmail.com>
2023-09-19 09:41:00 +0300

    implement solution for pick task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  const result = {};
 
+  for (const field of fields) {
+    if (!(field in obj)) continue;
+
+    result[field] = obj[field];
+  }
+
+  return result;
 };
