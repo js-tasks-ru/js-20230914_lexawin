4c1a7b48400e412fc82ab08026372847b304afdf
Aleksei Vinter <lexawin@gmail.com>
2023-09-29 00:10:09 +0300

    add solution for module 5 task 1: NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,58 @@
 export default class NotificationMessage {
+  static lastMessage = null;
 
+  constructor(message = "", options = {}) {
+    const { duration = 1000, type = "success" } = options;
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement();
+  }
+
+  createElement() {
+    const element = document.createElement("div");
+    element.classList.add("notification", this.type);
+    element.setAttribute(
+      "style",
+      `--value: ${Math.round(this.duration / 1000)}s;`
+    );
+
+    element.innerHTML = this.createElementContent();
+
+    return element;
+  }
+
+  createElementContent() {
+    return `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+    `;
+  }
+
+  destroy() {
+    clearTimeout(this.timer);
+    this.timer = null;
+    this.remove();
+    NotificationMessage.lastMessage = null;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  show(target = document.body) {
+    const lastMessage = NotificationMessage.lastMessage;
+    if (lastMessage) lastMessage.destroy();
+
+    NotificationMessage.lastMessage = this;
+
+    target.append(this.element);
+    this.timer = setTimeout(() => this.destroy(), this.duration);
+  }
 }
